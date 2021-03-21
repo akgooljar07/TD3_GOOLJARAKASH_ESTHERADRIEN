1ef310b6c3c74d2c0b0145cdcd4037989db74c9b
Master <agnetdesign@gmail.com>
2021-03-21 15:30:23 +0400

    selection

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
 
-<<<<<<< HEAD
-def mul(x,y):
-	return x*y
-=======
-def sum(x,y):
+
+def add(x,y):
 	return x+y
->>>>>>> 21256527c7d6e76b83cd4f5f9bcf27951c06132d
+
+def mul(x,y):
+    return x*y
 
 
 try:
@@ -15,25 +14,23 @@ try:
    num1,num2=(input("enter 2 number : ")).split( )
    w=int(num1)
    n=int(num2)
-   choice = input("Choose yes/no")
+   print("Choose 1 / 2 : ")
+   print("1.Addition")
+   print("2.Multiplication")
+   choice = int(input("Choose Your Operation : "))
 except (ValueError):
 
    print("Please input  2 numbers !")
    num1,num2=(input("enter 2 number : ")).split( )
    w=int(num1)
    n=int(num2)
-   choice = input("Choose yes/no  ")
+   choice = int(input("Choose Your Operation :  "))
 
 
-if (choice== "yes" ):
-<<<<<<< HEAD
-  	z=mul(w,n)
-  	print("The Multiplication : ",z)
-=======
-  	z=sum(w,n)
+if (choice== 1 ):
+  	z=add(w,n)
   	print("The sum : ",z)
->>>>>>> 21256527c7d6e76b83cd4f5f9bcf27951c06132d
 
 else:
-      print("1 num",w)
-      print("2 num ",n)
+    	m=mul(w,n)
+    	print("The Multiplication : ",m)
