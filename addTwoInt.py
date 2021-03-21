b7f217a588131df64aaaf88758650796a734a9b8
Master <agnetdesign@gmail.com>
2021-03-21 13:59:02 +0400

    [add]-Addition

---
+++ addTwoInt.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+def add(x,y):
+	return x+y
+
+def main():
+	num1 = int(input("Enter Num1: "))
+
+	num2 = int(input("Enter Num2: "))
+
+	z = add(num1,num2)
+
+	print("The SUM : ",z)
+
+main()
